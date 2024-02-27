95f6c39a2d2f9c97a6a1c445f302ae8cd0984ddc
sincerelyyyash <yta0088@gmail.com>
2024-02-28 02:42:26 +0530

    Blog card updated

--- frontend/src/Components/BlogCard.tsx
+++ frontend/src/Components/BlogCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { logo} from '../constants'
 
 interface BlogProps{
     title: string,
@@ -8,19 +9,22 @@ interface BlogProps{
 
 const BlogCard: React.FC<BlogProps> = ({title, description, author})=> {
   return (
-    <div className='flex flex-row m-12  w-2/5 h-40 rounded-md p-2'>
+    <div className='flex flex-row mr-12 ml-12 mt-12  w-3/5 h-40 rounded-md p-2'>
+        <div className='w-2/5'>
+            <a href="/">
+                <img src={logo} alt="Logo" width={120} height={120} />
+            </a>
+       </div>
        <div>
         <h2 className='text-xl font-montserrat font-bold p-1'>{title}</h2>
         <div>
             <p className='text-base font-palanquin p-1'>{description}</p>
         </div>
         <div>
-            <h6 className='text-base font-montserrat p-1'>{author}</h6>
+            <h6 className='text-base font-montserrat p-1'>~ {author}</h6>
         </div>
        </div>
-       {/* <div>
-        Image is here
-       </div> */}
+
     </div>
   )
 }
--- frontend/src/Pages/Homepage.tsx
+++ frontend/src/Pages/Homepage.tsx
@@ -4,12 +4,41 @@ import BlogCard from '../Components/BlogCard'
 
 function Homepage() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col mb-8'>
       <div>
       <Nav/>
       </div>
+      <div >
+      <BlogCard title='I Found 3 Passive Income Ideas Anyone Can Start'
+      description='Build your First or Next Passive Income Stream 
+      Today I know I’m not the only one who loves the idea of receiving payment
+      notifications without having to actively put in time. We only get 24 hours everyday.'
+      author='Paul Rose'
+      />
+      </div>
       <div>
-      <BlogCard/>
+      <BlogCard title='I Found 3 Passive Income Ideas Anyone Can Start'
+      description='Build your First or Next Passive Income Stream 
+      Today I know I’m not the only one who loves the idea of receiving payment
+      notifications without having to actively put in time. We only get 24 hours everyday.'
+      author='Paul Rose'
+      />
+      </div>
+      <div>
+      <BlogCard title='I Found 3 Passive Income Ideas Anyone Can Start'
+      description='Build your First or Next Passive Income Stream 
+      Today I know I’m not the only one who loves the idea of receiving payment
+      notifications without having to actively put in time. We only get 24 hours everyday.'
+      author='Paul Rose'
+      />
+      </div>
+      <div>
+      <BlogCard title='I Found 3 Passive Income Ideas Anyone Can Start'
+      description='Build your First or Next Passive Income Stream 
+      Today I know I’m not the only one who loves the idea of receiving payment
+      notifications without having to actively put in time. We only get 24 hours everyday.'
+      author='Paul Rose'
+      />
       </div>
     </div>
   )
