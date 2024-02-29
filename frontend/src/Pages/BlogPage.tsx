96cd4957b7b3bc1eae965f1364de6c3b41c4a6e9
sincerelyyyash <yta0088@gmail.com>
2024-03-01 00:58:57 +0530

    Blog page UI complete

--- frontend/src/Pages/BlogPage.tsx
+++ frontend/src/Pages/BlogPage.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
 import Nav from '../Components/Nav'
 
+type BlogPage = {
+  title: string,
+  subtitle: string,
+  content: string,
+  author: string,
+}
+
 function BlogPage({}) {
+const [title,setTitle]= useState("");
+const [subtite,setSubtitle]= useState("");
+const [content,setContent]= useState("");
+const [author,setAuthor]= useState("");
+
+
   return (
     <div className='flex flex-col mb-8'>
       <div>
@@ -9,25 +22,17 @@ function BlogPage({}) {
       </div>
       <div className='mt-14 flex flex-col justify-center items-center'>
         <h1 className='font-bold text-5xl w-2/4 font-palanquin'>
-        Top 15 Software Development Trends in 2024
+        {title}
         </h1>
         <div className='flex justify-center w-2/4 font-montserrat text-lg italic mt-8'>
-        As we step into 2024, the landscape of software development 
-        continues to evolve exponentially, driven by 
-        technological innovations and changing market needs.
+       {subtite}.
         </div>
         <div className='flex justify-items-start w-2/4'>
-            <h4 className='text-md font-montserrat p-1'>~ Author</h4>
+            <h4 className='text-md font-montserrat p-1'>~ {author}</h4>
         </div>
         <div className='w-2/4 mt-14 font-montserrat text-lg font-'>
             <p>
-            General principles of software engineering in 2024
-The general software development approaches set to define the IT industry in 2024 encompass further integration of AI and ML technologies, utilization of blockchain, and multi-runtime microservices. Expanded application of AR and VR will also continue to shape the industry. Additionally, programmers will put a greater emphasis on cybersecurity and sustainable software development. We explore each of these trends in detail in this section.
-
-Artificial intelligence and machine learning integration
-AI and machine learning are not buzzwords anymore; they’re integral components of modern software development, setting new standards for functionality and performance. From predictive algorithms to automated code reviews, AI/ML technologies are enhancing efficiency and capabilities across various industries.
-
-Among the most remarkable breakthroughs of 2023 was the introduction of the advanced ChatGPT features, which included capabilities for code and text generation, as well as significant development of AI-driven image creation based on text prompts.
+              {content}
             </p>
         </div>
       </div>
